cbc2a4433fe9e805bf29821b518465c37fa86ad8
Mike Peiman <mikepeiman@gmail.com>
2015-12-04 16:17:26 -0500

    got a jquery show/hide working on single tab/button, thanks to this simple example: http://crunchify.com/jquery-very-simple-showhide-panel-on-mouse-click-event/

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 // Accordion/Tabs from Bourbon Refills
+/*
 $(document).ready(function () {
   $('.accordion-tabs-minimal').each(function(index) {
     $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
   });
   $('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
-    if (!$(this).hasClass('is-active')) {
+    if (!$(this).hasClass('hidden')) {
       event.preventDefault();
       var accordionTabs = $(this).closest('.accordion-tabs-minimal');
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
+      $(this).next().toggleClass('hidden').toggle();
+      accordionTabs.find('hidden').removeClass('hidden').hide();
 
-      $(this).next().toggleClass('is-open').toggle();
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
+    
     } else {
       event.preventDefault();
+      $(this).addClass('hidden');
     }
   });
-});
+}); */
+
+  $(document).ready(function() {
+    $(".flip").click(function() {
+      $(".tab-content").slideToggle("slow");
+    });
+  });
 
 // Centered Top Navigation from Bourbon Refills
 $(document).ready(function() {
