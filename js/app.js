7a401e52e1aee8da50884e2df4f6bc35ffc46d02
Mike Peiman <mikepeiman@gmail.com>
2015-12-21 11:34:32 -0500

    working on final form styling

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
     });
   });
 
+
 // Centered Top Navigation from Bourbon Refills
 $(document).ready(function() {
   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
