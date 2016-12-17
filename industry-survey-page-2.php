26692693551f7faee05a53c3ec5ddf4c924ba76b
Mike Peiman <mikepeiman@gmail.com>
2016-12-17 17:07:08 -0500

    tiny tweaks experimenting, still not working

--- industry-survey-page-2.php
+++ industry-survey-page-2.php
@@ -47,10 +47,10 @@
                         <input name="infusionsoft_version" type="hidden" value="1.60.0.46" />
                     <fieldset>
                     <div class="infusion-field">
-                        <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="hidden" value="<?php echo $_GET[‘inf_field_FirstName’] ?>" />
+                        <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="hidden" value=<?php echo "$_GET[‘inf_field_FirstName’]" ?> />
                     </div>
                     <div class="infusion-field">
-                        <input class="infusion-field-input-container" id="inf_field_LastName" name="inf_field_LastName" type="hidden" value="<?php echo $_GET[‘inf_field_LastName’]?>" />
+                        <input class="infusion-field-input-container" id="inf_field_LastName" name="inf_field_LastName" type="hidden" value=<?php echo $_GET[‘inf_field_LastName’]?> />
                     </div>
                     <div class="infusion-field">
                         <input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="hidden" value="<?php echo $_GET[‘inf_field_Email’]?>" />
