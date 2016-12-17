d927b3ddb2f9b65bc2f16df7692c934c38368c9f
Mike Peiman <mikepeiman@gmail.com>
2016-12-17 16:58:55 -0500

    Got a tip on completing my php tag with <?php echo

--- industry-survey-page-2.php
+++ industry-survey-page-2.php
@@ -47,16 +47,16 @@
                         <input name="infusionsoft_version" type="hidden" value="1.60.0.46" />
                     <fieldset>
                     <div class="infusion-field">
-                        <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="hidden" value="<?=$_GET[‘inf_field_FirstName’] ?>" />
+                        <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="hidden" value="<?php echo $_GET[‘inf_field_FirstName’] ?>" />
                     </div>
                     <div class="infusion-field">
-                        <input class="infusion-field-input-container" id="inf_field_LastName" name="inf_field_LastName" type="hidden" value<?="$_GET[‘inf_field_LastName’]"?> />
+                        <input class="infusion-field-input-container" id="inf_field_LastName" name="inf_field_LastName" type="hidden" value="<?php echo $_GET[‘inf_field_LastName’]?>" />
                     </div>
                     <div class="infusion-field">
-                        <input id="inf_field_Email" name="inf_field_Email" type="hidden" value="<?=$_GET[‘inf_field_Email’]?>"/>
+                        <input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="hidden" value="<?php echo $_GET[‘inf_field_Email’]?>" />
                     </div>
                     <div class="infusion-field">
-                        <input class="infusion-field-input-container" id="inf_field_Phone1" name="inf_field_Phone1" type="hidden" value="<?=$_GET[‘inf_field_Phone1’]?>" />
+                        <input class="infusion-field-input-container" id="inf_field_Phone1" name="inf_field_Phone1" type="hidden" value="<?php echo $_GET[‘inf_field_Phone1’]?>" />
                     </div>
                     <div class="infusion-field">
                     <p>
